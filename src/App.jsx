812007a8dcbc67a4167cd8f2f931c817ec4c9904
Gabriel Gonçalves <103828336+GabrielFleckl@users.noreply.github.com>
2024-08-20 00:12:36 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,4 +25,5 @@ export default App;
 // Body: Criar seção para venda/aluguel de carros com cards dos carros e suas infos
 
 // implementar API mockada das infos dos carros
+
 // implementar pagina do admin onde pode realizar um CRUD com os carros 
